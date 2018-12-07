e3fc129aa0ee1455e3aa5407207c20a9034ef045
Alan Wong <captainalan@gmail.com>
2018-12-07 13:32:41 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Simple Bilingual Translation Page
+
+This website presents bilingual texts with hideable commentary in a
+minimalistic, mobile and desktop friendly manner.
+
+Data is read in via JSON. See a sample on Github Pages.
+
+## Other Notes
+
+This project is made with vanilla JavaScript (ES6) and HTML. 
+
+### Useful tools
+
+Use node's [serve](https://www.npmjs.com/package/serve) to ensure fetching JSON
+works. From the root directory:
+
+```
+serve -d .
+```
+
+Navigating your browser to the appropriate url (e.g. http://localhost:5000) will
+display the page.
+
---
+++ index.js
@@ -0,0 +1,66 @@
+// Languages used in bilingual document
+const LANGUAGE_FROM = 'jp'
+const LANGUAGE_TO   = 'en'
+
+// Fetch JSON data and render as HTML
+async function getText(){
+  fetch('./my_text.json')
+  .then(res => {
+    if (!res.ok) {
+      throw Error(res.statusText);
+    }
+    return res.json();
+  })
+  .then(myJSON => {
+    console.log(JSON.stringify(myJSON));
+
+    const elem = document.getElementById('muh-content')
+    let stuffToRender = myJSON.paragraphs.reduce((accumulator, currentElem) => {
+      console.log(Object.keys(currentElem));
+      return accumulator + '<div class="bilingual-box">'
+        + '<div class="language-from" lang=\''
+        + LANGUAGE_FROM
+        + '\'>'
+          + currentElem[LANGUAGE_FROM]
+        + '</div>'
+
+        + '<div class="language-to" lang=\''
+        + LANGUAGE_TO
+        + '\'>'
+          + currentElem[LANGUAGE_TO]
+        + '</div>'
+
+        + '<div class="commentary" lang=\''
+        + LANGUAGE_FROM
+        + '\'>'
+          + currentElem['comments']
+        + '</div>'
+      + '</div>'
+    }, '');
+
+    elem.innerHTML = stuffToRender;
+  })
+  .catch(error => {
+    console.log(error);
+    return {};
+  });
+}
+
+// Toggle commentary visibility
+let isHidden = false; // Initialize as false
+
+function toggleCommentary(){
+  elms = document.getElementsByClassName("commentary")
+  if (!isHidden) {
+    for (let i = 0; i < elms.length; i++) {
+      elms[i].style.display = "none";
+    }
+  } else {
+    for (let i = 0; i < elms.length; i++) {
+      elms[i].style.display = "block";
+    }
+  }
+  isHidden = !isHidden; // Toggle value
+}
+
+
